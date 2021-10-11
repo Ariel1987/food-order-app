4c8e9e921c80edb0adef9ae9fc8e6333ff0244c3
Ariel <arielpiresdealmeida@gmail.com>
2021-10-11 18:28:56 -0300

    chore: add inputs to checkout form and send to firebase

--- src/components/CheckOutForm/CheckOutForm.styles.js
+++ src/components/CheckOutForm/CheckOutForm.styles.js
@@ -2,60 +2,56 @@ import styled, { css } from "styled-components";
 
 export const InputWrapper = styled.div`
     ${({ theme }) => css`
-    margin: 1rem 0;
-    height: 19rem;
-    overflow: auto;
+        margin: 1rem 0;
+        height: 19rem;
+        overflow-y: scroll;
 
-    input {
-        margin-bottom: 0.5rem;
-    }
+        input {
+            margin-bottom: 0.5rem;
+        }
 
-    label {
-        font-weight: bold;
-        margin-bottom: 0.25rem;
-        display: block;
-    }
+        label {
+            font-weight: bold;
+            margin-bottom: 0.25rem;
+            display: block;
+        }
 
-    input {
-        font: inherit;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        width: 20rem;
-        max-width: 100%;
-    }
+        input {
+            font: inherit;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            width: 20rem;
+            max-width: 100%;
+            border-color: ${props => props.borderColor ? '#aa0b20': ''}
+        }
 
-    section {
-        display: flex;
-        justify-content: flex-end;
-        gap: 1rem;
-    }
+        section {
+            display: flex;
+            justify-content: flex-end;
+            gap: 1rem;
+        }
 
-
-    .invalid label {
-    color: #ca3e51;
-    }
-
-    .invalid input {
-    border-color: #aa0b20;
-    background-color: #ffeff1;
-    }
-
-
+        p {
+            color: #aa0b20;
+        }
     `}
 `
 
 export const SubmitButton = styled.div`
     ${({ theme }) => css`{
-        font: inherit;
-        cursor: pointer;
-        border-radius: 25px;
-        padding: 0.5rem 2rem; 
-        border: 1px solid #5a1a01;
-        background-color: #5a1a01;
-        color: white;
-        :hover,
-        :active {
-            background-color: #7a2706;        
+        button{
+            font: inherit;
+            cursor: pointer;
+            border-radius: 25px;
+            padding: 0.5rem 2rem; 
+            border: 1px solid #5a1a01;
+            background-color: #5a1a01;
+            color: white;
+            :hover,
+            :active {
+                background-color: #7a2706;        
+            }
+
         }
     `}
 `
